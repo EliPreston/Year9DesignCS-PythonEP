9a7522d3f69bc2ecdee88ac789d9baa3a3ff60f1
Eli Preston <eli.preston@Y22-Design-eli-preston.local>
2018-10-05 11:04:37 -0400

    window attempt

---
+++ GUIAddingJPG.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+#This creates the main window of an application
+window = tk.Tk()
+window.title("Join")
+window.geometry("600x780")
+window.configure(background='grey')
+
+
+#Start the GUI
+window.mainloop() 
