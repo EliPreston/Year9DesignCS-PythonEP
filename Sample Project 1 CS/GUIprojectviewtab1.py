59396a62e7debbabc8c8042d1c23ed5e8df011ab
Eli Preston <eli.preston@Subscribe-To-PewDiePie.local>
2018-12-17 11:07:39 -0500

    Last push

--- Sample Project 1 CS/GUIprojectviewtab1.py
+++ Sample Project 1 CS/GUIprojectviewtab1.py
@@ -2,97 +2,208 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 from PIL import ImageTk, Image
+import random
+
+
+def start(*args):
+	print("Starting Quiz")
+	print("Other Information:")	
+	outputOI.config(state="normal")
+	outputP.config(state="normal")
+	outputH.config(state="normal")
+
+
+	randomelement = random.randint(0,3)
+
+	data[0] = random.randint(0,len(fileName) - 1)
+
+	outputValue = (otherinformation[data[0]])
+
+	outputOI.delete(1.0,tk.END)
+	outputOI.insert(tk.INSERT,outputValue)
+	outputOI.config(state="disabled")
+
+
+	outputValue1 = (points)
+
+	outputP.delete(1.0,tk.END)
+	outputP.insert(tk.INSERT,points)
+	outputP.config(state="disabled")	
+
+	outputH.delete(1.0,tk.END)
+	outputH.config(state="disabled")
+
+	print(data[0])
+	#LOAD YOUR IMAGE BASED ON fileName[num]
+
+	#img = ImageTk.PhotoImage(Image.open(fileName[num]))
+	#panel.config(image = img)
 
 
 
 
-def submit1():
-	print("submit")
+
+
+def submitA(*args):	
+	labA.config(foreground="purple", background="black")
+	outputRW.config(state="normal")
+	outputP.config(state="normal")
+	print("You have answered")
+	print(entA.get())
+	
+	#pointsDefault = 0
+
+	value = entA.get()
+
+
+	if value == data[0]:
+
+		outputValue1 = ("Correct")
+		outputRW.delete(1.0,tk.END)
+		outputRW.insert(tk.INSERT,outputValue1)
+		outputRW.config(state="disabled")
+
+	else:
+		outputValue = ("Wrong")
+		outputRW.delete(1.0,tk.END)
+		outputRW.insert(tk.INSERT,outputValue)
+		outputRW.config(state="disabled")
+
+	loc = -1
+
+	
+
+
+	#for i in range (0, len(elements), 1):
+	#	if elements[i] == value:
+	#		loc = [i]
+
+	#	if loc != -1:
+	#		outputValue1 = ("Correct")
+	#		outputRW.delete(1.0,tk.END)
+	#		outputRW.insert(tk.INSERT,outputValue1)
+	#		outputRW.config(state="disabled")
+			
+		
+			#outputP.delete(1.0,tk.END)
+			#outputP.insert(tk.INSERT,pointsDefault+1)
+			#outputRW.config(state="disabled")
+
+	#	else:
+	#		outputValue = ("Wrong")
+	#		outputRW.delete(1.0,tk.END)
+	#		outputRW.insert(tk.INSERT,outputValue)
+	#		outputRW.config(state="disabled")
+
+
+def Hint(*args):
+	print("Getting Hint")	
+	outputH.config(state="normal")
+
+	print("*****************"+str(data[0])+"*****************")
+	outputValue = (description[data[0]])
+	
+
+	outputH.delete(1.0,tk.END)
+	outputH.insert(tk.INSERT,outputValue)
+	outputH.config(state="disabled")
 	
 
 
 
-
-
-
+#*******************************************
 
 root = tk.Tk()
-root.title("The Elements Study Tool")
-root.geometry("600x600")
-root.configure(background= 'grey' )
-root.resizable(False,False)
+root.title("Elements Study Tool")
+root.configure(background="#90AFC5")
+root.resizable(False, False)
+
+#*******************************************
+
+points = 0
+
+#data is list that stores state data,  data[0] is my randomized num
+data = [0]
+
+fileName = ["1_Hydrogen.jpg","2_Helium.jpg","3_Lithium.jpg"]
+
+otherinformation = ["Used in rockets", "Used in party balloons", "Lithium other information"]
+
+elements = ["Hydrogen", "Helium", "Lithium"]
+
+description = ["This is used in rocket fuel", "This has to do with party balloons", "Lithium hint"]
 
 
 
 
-labT = tk.Label(root, text= "The Periodic Table Quiz")
-labT.config(background= 'grey')
-labT.grid(row = 0, column = 0, columnspan = 2)
+#****************** COLUMN 1 #******************
+labT = tk.Label(root, text = "Periodic Quiz", background = "#0366D6", foreground = "white", height = 2)
+labT.config()
+labT.grid(row = 0, column = 0, columnspan = 2, sticky= "w")
 
-btnS = tk.Button(root, text="Start")
+btnS = tk.Button(root, text="Quiz Me!")
 btnS.grid(row = 1, column = 0, sticky = "w")
+btnS.bind("<Button-1>",start)
 
-labP = tk.Label(root, text= "Points During Quiz")
-labP.config(background= 'grey')
+labP = tk.Label(root, text= "Points")
+labP.config(background= '#90AFC5')
 labP.grid(row = 2, column = 0, columnspan = 2, sticky = "w")
 
 outputP = tk.Text(root, width=10, height=1, borderwidth=3, relief=tk.GROOVE)
 outputP.config(state="disabled", background= 'grey')
 outputP.grid(row = 3, column = 0, sticky = "w")
 
-labFP = tk.Label(root, text= "Final Score")
-labFP.config(background= 'grey')
-labFP.grid(row = 4, column = 0, columnspan = 2, sticky = "w")
-
-outputFS = tk.Text(root, width=10, height=1, borderwidth=3, relief=tk.GROOVE)
-outputFS.config(state="disabled", background= 'grey')
-outputFS.grid(row = 5, column = 0, sticky = "w")
 
 
-#********* COLUMN 2/3 ******************
-
-labT = tk.Label(root, text= "This will be a timer")
-labT.config(background= 'grey')
-labT.grid(row = 1, column = 3, columnspan = 2, sticky = "w")
-
-labO = tk.Label(root, text= "Other Information")
-labO.config(background= 'grey')
-labO.grid(row = 2, column = 3, columnspan = 3, sticky = "w")
-
-outputP = tk.Text(root, width=10, height=1, borderwidth=3, relief=tk.GROOVE)
-outputP.config(state="disabled", background= 'grey')
-outputP.grid(row = 3, column = 0, sticky = "w")
-
-btnH = tk.Button(root, text="Hint?", command=submit1)
-btnH.config()
-btnH.grid(row = 4, column = 3, sticky = "w")
-
-output = tk.Text(root, width=10, height=1, borderwidth=3, relief=tk.GROOVE)
-output.config(state="disabled", background= 'grey')
-output.grid(row = 3, column = 0, sticky = "w")
-
-#********* COLUMN 6 ******************
-
-#path = "1_Hydrogen.jpg"
-#img = ImageTk.PhotoImage(Image.open(path))
-#panel = tk.Label(root, image = img)
-#panel.grid(row = 1, column = 6, side = "bottom", fill = "both", expand = "yes")
 
 
-labA = tk.Label(root, text= "Your Answer")
-labA.config(background= 'grey')
+
+#****************** COLUMN 2/3 #******************
+#labT = tk.Label(root, text= "This will be a timer")
+#labT.config(background= 'grey')
+#labT.grid(row = 1, column = 3, columnspan = 2, sticky = "w")
+
+labOI = tk.Label(root, text= "Other Information")
+labOI.config(background= '#90AFC5')
+labOI.grid(row = 2, column = 3, columnspan = 3, sticky = "w")
+#labOI.bind("<Button-1>",start)
+
+outputOI = tk.Text(root, width=15, height=3, borderwidth=3, relief=tk.GROOVE)
+outputOI.config(state="disabled", background= 'grey')
+outputOI.grid(row = 3, column = 3, sticky = "w")
+
+labH = tk.Button(root, text="Hint?")
+#labH.config()
+labH.grid(row = 4, column = 3, sticky = "w")
+labH.bind("<Button-1>",Hint)
+
+outputH = tk.Text(root, width=15, height=3, borderwidth=3, relief=tk.GROOVE)
+outputH.config(state="disabled", background= 'grey')
+outputH.grid(row = 5, column = 3, sticky = "w")
+
+
+
+
+
+
+#****************** COLUMN 6 #******************
+num = random.randint(0,len(fileName) - 1)
+img = ImageTk.PhotoImage(Image.open(fileName[num]))
+
+panel = tk.Label(root)
+panel.grid(row = 1, column = 6)
+
+
+labA = tk.Button(root, text= "Your Answer", command = submitA)
+#labA.config(fg= '#DB4437')
 labA.grid(row = 2, column = 6, sticky = "w")
+#labA.bind("<Button-1>",submitA)
 
 entA = tk.Entry(root, width=15, borderwidth=3)
 entA.config(background= 'grey')
 entA.grid(row = 3, column = 6, sticky = "w")
 
-
-labRA = tk.Label(root, text= "This will output a right or wrong text")
-labRA.config(background= 'grey')
-labRA.grid(row = 4, column = 6, sticky = "w")
-
-outputRW = tk.Text(root, width = 10, height = 1, borderwidth=3, relief=tk.GROOVE)
+outputRW = tk.Text(root, width = 15, height = 3, borderwidth=3, relief=tk.GROOVE)
 outputRW.config(state = "disabled", background= 'grey')
 outputRW.grid(row = 5, column = 6, sticky = "w")
 
@@ -100,18 +211,10 @@ outputRW.grid(row = 5, column = 6, sticky = "w")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 root.mainloop()
+
+
+
 
 
 
